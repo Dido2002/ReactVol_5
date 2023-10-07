c2d854236ea750578b85ea684b9b11d16041db68
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-07 20:06:11 +0300

    Add files via upload

---
+++ App02.js
@@ -0,0 +1,12 @@
+import React,{useState, useEffect} from "react";
+import BookManagement from "./BookManagment";
+
+export default function App02(){
+
+
+    return(
+        <>
+            <BookManagement/>
+        </>
+    )
+}
