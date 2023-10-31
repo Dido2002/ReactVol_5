f661d117c3c71e86158285ecb03be4dda34d0e70
Dido <100509456+Dido2002@users.noreply.github.com>
2023-10-31 18:07:52 +0200

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import withMainLayout from "../layout/withMainLayout";
+
+function Home(){
+    return(
+        <>
+        1231
+        </>
+    )
+}
+export default withMainLayout(Home)
